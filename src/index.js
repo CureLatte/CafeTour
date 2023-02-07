5ba01f7001c7db74b17b4a1911ffb044192ee884
zhqmfkv <zhqmfkv@naver.com>
2023-02-07 21:44:50 +0900

    [FEAT] context

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import MouseApp from "./MouseApp/MouseApp";
 import AppMentor from "./mentorApp/AppMentor";
 import AppMentors from "./mentorsApp/AppMentors"
 import AppMentorsImmer from "./mentorsApp/AppMentorsImmer";
+import AppTheme from "./AppTheme/AppTheme";
 
 // 전역 설정이 가능함
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <AppMentorsImmer />
+        <AppTheme />
     </React.StrictMode>
 );
 reportWebVitals();
