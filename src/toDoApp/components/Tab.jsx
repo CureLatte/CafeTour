0a78baca0057d43c4a9bd824e486e58162cae4c3
zhqmfkv <zhqmfkv@naver.com>
2023-02-18 23:14:30 +0900

    [FEAT] tabBar setting

---
+++ src/toDoApp/components/Tab.jsx
@@ -0,0 +1,25 @@
+import style from "../css/header.module.css";
+import {useEffect, useRef} from "react";
+
+export default function Tab({title}){
+    const tabTitle = useRef(null)
+    const tabBar = useRef(null)
+
+
+
+    useEffect(() => {
+        // px 길이 세팅
+        tabBar.current.style.width=tabTitle.current.clientWidth.toString() + "px"
+        return () => {
+
+        };
+    }, []);
+
+
+    return (
+        <span className={style.tabs} ref={tabTitle}>
+            <p>{title}</p>
+            <div className={style.tabsBar} ref={tabBar}/>
+        </span>
+    )
+}
--- src/toDoApp/components/header.jsx
+++ src/toDoApp/components/header.jsx
@@ -1,4 +1,5 @@
 import style from  '../css/header.module.css'
+import Tab from './Tab.jsx'
 
 export default function Header(){
 
@@ -7,19 +8,9 @@ export default function Header(){
         <div className={style.header}>
             <img src='/images/darkMode.png' alt="error" className={style.darkMode}/>
             <div className={style.tabsContainer}>
-                <span className={style.tabs}>
-                    <p>All</p>
-                    <div className={style.tabsBar}/>
-                </span>
-
-                <span className={style.tabs}>
-                    <p>active</p>
-                    <div className={style.tabsBar}/>
-                </span>
-                <span className={style.tabs}>
-                    <p>completed</p>
-                    <div className={style.tabsBar}/>
-                </span>
+                <Tab title="All"></Tab>
+                <Tab title="Activate"></Tab>
+                <Tab title="Completed"></Tab>
             </div>
         </div>
     )
